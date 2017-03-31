f7c5576a2369309d1b5b48f6e1ebf2985ffc81a8
Erik Steenman <eriksteenman@gmail.com>
2017-03-31 11:20:58 +0000

    Add an (empty) separate ticker page

--- client/renderRoutes.jsx
+++ client/renderRoutes.jsx
@@ -4,6 +4,7 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from '/client/App.jsx';
 
 import LoginPage from '/client/Login.jsx';
+import TickerPage from '/client/Ticker.jsx';
 import ManagerPage from '/client/Manager.jsx';
 import AuthenticatedContainer from '/client/containers/AuthenticatedContainer.jsx';
 
@@ -13,8 +14,9 @@ export default renderRoutes = () => (
 	<Router history={browserHistory}>
 		<Route component={App}>
 			<Route path="/login" component={LoginPage} />
+			<Route path="/" component={TickerPage} />
 			<Route component={AuthenticatedContainer}>
-				<Route path="/" component={ManagerPage} />
+				<Route path="/manage" component={ManagerPage} />
 			</Route>
 		</Route>
 	</Router>
